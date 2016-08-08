ac014c92a79b9f106b9a52cce2e52d6e498b11d0
mycspring <1248036495@qq.com>
2016-08-08 22:49:47 +0800

    wav reader

---
+++ MusicReader/DisplaySample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WavReader;
+
+namespace DisplaySample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new Form();
+
+            form.Paint += new PaintEventHandler(paintHandler);
+            Application.Run(form);
+        }
+
+        public static void paintHandler(Object sender, PaintEventArgs e)
+        {
+            WavFile wavFile = new WavFile("t.wav");
+            int length = (wavFile.length < 1000) ? wavFile.length : 1000;
+            Pen blackPen = new Pen(Color.Black, 1);
+            double gap = 1000.0 / length;
+            Point p1 = new Point(0, 303 - wavFile.data[0]);
+            Point p2 = p1;
+            for (int i = 1; i < length; ++i)
+            {
+                p1 = p2;
+                p2 = new Point((int)(i * gap), 303 - wavFile.data[i]);
+                e.Graphics.DrawLine(blackPen, p1, p2);
+            }
+        }
+    }
+}
---
+++ MusicReader/WavReader/WavFile.cs
@@ -0,0 +1,29 @@
+﻿using NAudio.Wave;
+
+/// <summary>
+/// read wav file
+/// </summary>
+namespace WavReader
+{
+    public class WavFile
+    {
+        //chunk data of wav file
+        public byte[] data { get; set; }
+
+        //length of data
+        public int length { get; set; }
+
+        /// <summary>
+        /// read wav file
+        /// </summary>
+        /// <param name="filename">wav file name</param>
+        public WavFile(string filename)
+        {
+            var fileReader = new WaveFileReader(filename);
+            var wavStream = WaveFormatConversionStream.CreatePcmStream(fileReader);
+            length = (int)wavStream.Length;
+            data = new byte[length];
+            wavStream.Read(data, 0, length);
+        }
+    }
+}
