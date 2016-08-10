f459cc450f8f853b2ffa19310f6232dba81f01d2
Zheng Guo <t-zhg@microsoft.com>
2016-08-10 11:12:53 +0800

    Try to push the first time

---
+++ BackEnd/Controllers/ChordController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MrChorder.Controllers
+{
+    public class ChordController : Controller
+    {
+        //[HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void GetFile(HttpPostedFileBase file)
+        {
+            string path = System.IO.Path.Combine(Server.MapPath("~/Images"), System.IO.Path.GetFileName(file.FileName));
+            file.SaveAs(path);
+            ViewBag.Message = "File uploaded successfully";
+        }
+    }
+}
