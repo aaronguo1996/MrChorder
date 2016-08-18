78d7087e37583ad4064bffa0a3861798381c1051
Yao Xie <t-yaxie@microsoft.com>
2016-08-18 15:13:21 +0800

    bianjiao

--- PDF/Training/Program.cs
+++ PDF/Training/Program.cs
@@ -16,7 +16,7 @@ namespace Training
     {
         static void Main(string[] args)
         {
-            DataTable table = new ExcelReader("C:/Users/t-yaxie/Desktop/Fun/single_note_result/result_without_similar_freq.xls").GetWorksheet("Sheet1");
+            DataTable table = new ExcelReader("C:/Users/t-yaxie/Desktop/Fun/single_note_result/result.xls").GetWorksheet("Sheet1");
             DataTable test_table = new ExcelReader("C:/Users/t-yaxie/Desktop/Fun/single_note_result/result_star_50.xls").GetWorksheet("Sheet1");
             //[index][features] featrues: [p1 p2 p3 p4]
             double[][] inputs = table.ToArray<double>("freq1", "freq2", "freq3", "freq4", "freq5");
